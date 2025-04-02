fe76bb7b465dd4d8c40b69384214b57ab3799a24
odtboun <odtboun@users.noreply.github.com>
2025-04-02 12:08:23 +0300

    fix: remove web dependencies to resolve build issues

---
+++ app.config.js
@@ -0,0 +1,39 @@
+export default {
+  expo: {
+    name: "Simple-Flashcard-App",
+    slug: "Simple-Flashcard-App",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#000000"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.omerdem.simpleflashcard",
+      buildNumber: "1.0.0",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#000000"
+      }
+    },
+    extra: {
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      eas: {
+        projectId: "dbf288a2-6f5f-4eea-bf7f-f4681b1fd860"
+      }
+    },
+  }
+}; 
