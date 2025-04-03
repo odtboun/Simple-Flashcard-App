2968b3c05be39d58d218bf322a23cd8ad261e62f
odtboun <odtboun@users.noreply.github.com>
2025-04-03 07:46:59 +0300

    chore: remove icon and splash screen config temporarily

--- app.config.js
+++ app.config.js
@@ -4,13 +4,7 @@ export default {
     slug: "Simple-Flashcard-App",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#000000"
-    },
     assetBundlePatterns: [
       "**/*"
     ],
